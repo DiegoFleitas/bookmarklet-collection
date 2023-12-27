8afa0d1cde95d01b9ee317b25a265b691b6bf539
Diego Fleitas <diego.fleitas68@gmail.com>
2023-12-27 10:41:42 -0300

    add justwatch watchlist exporter

---
+++ justwatch-watchlist-exporter/index.js
@@ -0,0 +1,87 @@
+javascript: (function () {
+    // PRE: switch to ENG language on JustWatch website if not already
+    // PRE: allow emergent windows on browser if popup appears
+    async function openQuick(link) {
+        let win = window.open(link, '_blank');
+        await sleep(10000);
+        if (win) {
+            console.log("Window opened");
+            // Inject the scrapeMovieData function into the new window
+            let movieData = win.eval(`(${scrapeMovieData.toString()})();`);
+            // Save to local storage
+            let watchlistData = JSON.parse(localStorage.getItem("watchlistData") || "[]");
+            watchlistData.push(movieData);
+            localStorage.setItem("watchlistData", JSON.stringify(watchlistData));
+            // Close tab
+            win.close();
+        }
+    }
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    function getLinks() {
+        var output = [];
+        document.querySelectorAll("h2.title-card-heading").forEach((e) => {
+            if (e.parentNode.href) {
+                output.push(e.parentNode.href);
+            }
+        });
+        return output;
+    }
+
+    async function main() {
+        var links = getLinks();
+        console.log(links);
+        for (let i = 0; i < links.length; i++) {
+            console.log(links[i]);
+            await openQuick(links[i]);
+        }
+        console.log(localStorage.getItem("watchlistData"));
+    }
+
+    function scrapeMovieData() {
+        const out = [];
+        const genreMap = new Map();
+        const defaultClient = __APOLLO_STATE__.defaultClient;
+    
+        const keys = Object.keys(defaultClient).filter(k => k.match(/Movie.*content.*\)$/));
+        const movieKey = keys.reduce((a, b) => Object.keys(defaultClient[a]).length > Object.keys(defaultClient[b]).length ? a : b);
+        const movie = defaultClient[movieKey];
+    
+        const externalIds = Object.keys(defaultClient).filter(k => k.match(/Movie.*content.*\).externalIds$/));
+        const imdbid = externalIds.length > 0 ? defaultClient[externalIds.find(k => "imdbId" in defaultClient[k])].imdbId : "";
+    
+        const genreKeys = Object.keys(defaultClient).filter(k => k.match(/^Genre:/));
+        genreKeys.forEach(genreKey => {
+            const genreObj = defaultClient[genreKey];
+            genreMap.set(genreObj.shortName, genreObj["slug({\"language\":\"en\"})"]);
+        });
+    
+        const creditKeys = movie.credits.map(credit => credit.id);
+        const directors = creditKeys.map(k => defaultClient[k]).filter(credit => credit.role === "DIRECTOR").map(credit => credit.name);
+        directors.sort();
+    
+        const title = movie.title;
+        const ogtitle = movie.originalTitle;
+        const genres = movie.genres.map(genre => genreMap.get(defaultClient[genre.id].shortName));
+    
+        const duration = movie.runtime;
+        out.push({
+            englishTitle: title,
+            originalTitle: ogtitle,
+            director: directors,
+            imdbID: imdbid,
+            releaseYear: movie.originalReleaseYear,
+            releaseDate: movie.originalReleaseDate || "",
+            runtime: duration,
+            ageCertification: movie.ageCertification,
+            genres: genres.sort()
+        });
+    
+        return out;
+    }
+
+    main();
+})()
