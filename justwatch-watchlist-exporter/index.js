60a509a2ac19876696372a1e4cd774a5fb1360c2
Diego Fleitas <diego.fleitas68@gmail.com>
2024-04-10 23:24:02 -0300

    update script

--- justwatch-watchlist-exporter/index.js
+++ justwatch-watchlist-exporter/index.js
@@ -5,65 +5,254 @@ javascript: (async function () {
 
     async function fetchAndParseData(link) {
         try {
-            const response = await fetch(link);
-            const html = await response.text();
-            return parseApolloState(html);
+            const myHeaders = new Headers();
+            myHeaders.append("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0");
+            myHeaders.append("Accept", "*/*");
+            myHeaders.append("Accept-Language", "es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3");
+            myHeaders.append("Accept-Encoding", "gzip, deflate, br");
+            myHeaders.append("Referer", "https://www.justwatch.com/");
+            myHeaders.append("content-type", "application/json");
+            myHeaders.append("Origin", "https://www.justwatch.com");
+            myHeaders.append("Authorization", `Bearer ${document.cookie.replace(/(?:(?:^|.*;\s*)access_token\s*\=\s*([^;]*).*$)|^.*$/, "$1")}`);
+            myHeaders.append("Host", "apis.justwatch.com");
+            myHeaders.append("App-Version", "3.8.2-web-web");
+            /* myHeaders.append("DEVICE-ID", `${document.cookie.replace(/(?:(?:^|.*;\s*)jw_id\s*\=\s*([^;]*).*$)|^.*$/, "$1")}`); */
+            /* myHeaders.append("Jw-Session", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWRpZW5jZSI6InNlc3Npb24iLCJleHAiOjMyMjY1MjU4NTYsImlhdCI6MTcxMjc5Nzg1NiwiaXNzdWVyIjoianVzdHdhdGNoIiwic3ViIjoiNkRMaTA2czVFZTJpTmRyMW1LU1ZsZyJ9.Qo2PxYh3ca57q7OMqmc0Y0An_MLhQQc5W-StCka7RhM"); */
+            myHeaders.append("App-Version", "3.8.2-web-web");
+            myHeaders.append("Origin", "https://www.justwatch.com");
+            myHeaders.append("Sec-Fetch-Site", "same-site");
+            myHeaders.append("Connection", "keep-alive");
+            myHeaders.append("Sec-Fetch-Dest", "empty");
+            myHeaders.append("Sec-Fetch-Mode", "cors");
+            myHeaders.append("Sec-Fetch-Site", "same-site");
+
+            const graphql = JSON.stringify({
+                query: `
+                    query GetTitleListV2(
+                        $country: Country!, 
+                        $titleListFilter: TitleFilter, 
+                        $titleListSortBy: TitleListSortingV2! = LAST_ADDED, 
+                        $titleListType: TitleListTypeV2!, 
+                        $titleListAfterCursor: String, 
+                        $watchNowFilter: WatchNowOfferFilter!, 
+                        $first: Int! = 10, 
+                        $language: Language!, 
+                        $sortRandomSeed: Int! = 0, 
+                        $profile: PosterProfile, 
+                        $backdropProfile: BackdropProfile, 
+                        $format: ImageFormat, 
+                        $platform: Platform! = WEB, 
+                        $includeOffers: Boolean = false
+                    ) {
+                        titleListV2(
+                            after: $titleListAfterCursor
+                            country: $country
+                            filter: $titleListFilter
+                            sortBy: $titleListSortBy
+                            first: $first
+                            titleListType: $titleListType
+                            sortRandomSeed: $sortRandomSeed
+                        ) {
+                            totalCount
+                            pageInfo {
+                                startCursor
+                                endCursor
+                                hasPreviousPage
+                                hasNextPage
+                                __typename
+                            }
+                            edges {
+                                ...WatchlistTitleGraphql
+                                __typename
+                            }
+                            __typename
+                        }
+                    }
+                    fragment WatchlistTitleGraphql on TitleListEdgeV2 {
+                        cursor
+                        node {
+                            id
+                            objectId
+                            objectType
+                            offerCount(country: $country, platform: $platform)
+                            offers(country: $country, platform: $platform) @include(if: $includeOffers) {
+                                id
+                                presentationType
+                                monetizationType
+                                retailPrice(language: $language)
+                                type
+                                package {
+                                    id
+                                    packageId
+                                    clearName
+                                    __typename
+                                }
+                                standardWebURL
+                                elementCount
+                                deeplinkRoku: deeplinkURL(platform: ROKU_OS)
+                                __typename
+                            }
+                            content(country: $country, language: $language) {
+                                title
+                                fullPath
+                                originalReleaseYear
+                                shortDescription
+                                scoring {
+                                    imdbScore
+                                    imdbVotes
+                                    tmdbScore
+                                    tmdbPopularity
+                                    __typename
+                                }
+                                externalIds {
+                                    imdbId
+                                    tmdbId
+                                    __typename
+                                }
+                                posterUrl(profile: $profile, format: $format)
+                                backdrops(profile: $backdropProfile, format: $format) {
+                                    backdropUrl
+                                    __typename
+                                }
+                                upcomingReleases(releaseTypes: [DIGITAL]) {
+                                    releaseDate
+                                    __typename
+                                }
+                                isReleased
+                                __typename
+                            }
+                            likelistEntry {
+                                createdAt
+                                __typename
+                            }
+                            dislikelistEntry {
+                                createdAt
+                                __typename
+                            }
+                            watchlistEntryV2 {
+                                createdAt
+                                __typename
+                            }
+                            customlistEntries {
+                                createdAt
+                                __typename
+                            }
+                            watchNowOffer(country: $country, platform: $platform, filter: $watchNowFilter) {
+                                id
+                                standardWebURL
+                                package {
+                                    id
+                                    packageId
+                                    clearName
+                                    __typename
+                                }
+                                retailPrice(language: $language)
+                                retailPriceValue
+                                currency
+                                lastChangeRetailPriceValue
+                                presentationType
+                                monetizationType
+                                availableTo
+                                __typename
+                            }
+                            ... on Movie {
+                                seenlistEntry {
+                                    createdAt
+                                    __typename
+                                }
+                                __typename
+                            }
+                            ... on Show {
+                                tvShowTrackingEntry {
+                                    createdAt
+                                    __typename
+                                }
+                                seenState(country: $country) {
+                                    seenEpisodeCount
+                                    releasedEpisodeCount
+                                    progress
+                                    caughtUp
+                                    lastSeenEpisodeNumber
+                                    lastSeenSeasonNumber
+                                    __typename
+                                }
+                                __typename
+                            }
+                            __typename
+                        }
+                        __typename
+                    }
+                `,
+                variables: {
+                    "titleListSortBy": "LAST_ADDED",
+                    "first": 20,
+                    "sortRandomSeed": 0,
+                    "platform": "WEB",
+                    "includeOffers": false,
+                    "titleListFilter": {
+                        "ageCertifications": [],
+                        "excludeGenres": [],
+                        "excludeProductionCountries": [],
+                        "objectTypes": ["MOVIE"],
+                        "productionCountries": [],
+                        "subgenres": [],
+                        "genres": [],
+                        "packages": [],
+                        "excludeIrrelevantTitles": false,
+                        "presentationTypes": [],
+                        "monetizationTypes": [],
+                        "includeTitlesWithoutUrl": true
+                    },
+                    "watchNowFilter": {
+                        "packages": [],
+                        "monetizationTypes": []
+                    },
+                    "language": "en-US",
+                    "country": "US",
+                    "titleListType": "WATCHLIST",
+                    "titleListAfterCursor": ""
+                }
+            })
+            const requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: graphql,
+                redirect: 'follow'
+            };
+
+            const response = await fetch("https://apis.justwatch.com/graphql", requestOptions);
+            const data = await response.json();
+            return data.data.titleListV2.edges.map(edge => {
+                const movie = edge.node.content;
+
+
+                const title = movie.title;
+                const ogtitle = movie.title;
+                const directors = [];
+                const imdbid = movie.externalIds.imdbId;
+                const duration = null;
+
+
+                return {
+                    englishTitle: title,
+                    originalTitle: ogtitle,
+                    director: directors,
+                    imdbID: imdbid,
+                    releaseYear: movie.originalReleaseYear,
+                    releaseDate: "",
+                    runtime: duration,
+                    ageCertification: null,
+                    genres: []
+                };
+            });
         } catch (error) {
             console.error('Error fetching URL:', link, error);
             return null;
         }
     };
 
-    function parseApolloState(html) {
-        const doc = new DOMParser().parseFromString(html, 'text/html');
-        const scriptTags = Array.from(doc.querySelectorAll('script'));
-        const apolloStateScript = scriptTags.find(script => script.textContent.includes('__APOLLO_STATE__'));
-        if (!apolloStateScript) return null;
-    
-        const stateText = apolloStateScript.textContent;
-        const stateJson = stateText.substring(stateText.indexOf('{'), stateText.lastIndexOf('}') + 1);
-        const state = JSON.parse(stateJson);
-
-        const defaultClient = state.defaultClient;
-    
-        const keys = Object.keys(defaultClient).filter(k => k.match(/Movie.*content.*\)$/));
-        const movieKey = keys.reduce((a, b) => Object.keys(defaultClient[a]).length > Object.keys(defaultClient[b]).length ? a : b);
-        const movie = defaultClient[movieKey];
-    
-        const externalIds = Object.keys(defaultClient).filter(k => k.match(/Movie.*content.*\).externalIds$/));
-        const imdbid = externalIds.length > 0 ? defaultClient[externalIds.find(k => "imdbId" in defaultClient[k])].imdbId : "";
-    
-        const genreMap = new Map();
-        const genreKeys = Object.keys(defaultClient).filter(k => k.match(/^Genre:/));
-        genreKeys.forEach(genreKey => {
-            const genreObj = defaultClient[genreKey];
-            genreMap.set(genreObj.shortName, genreObj["slug({\"language\":\"en\"})"]);
-        });
-    
-        const creditKeys = movie.credits.map(credit => credit.id);
-        const directors = creditKeys.map(k => defaultClient[k]).filter(credit => credit.role === "DIRECTOR").map(credit => credit.name);
-        directors.sort();
-    
-        const title = movie.title;
-        const ogtitle = movie.originalTitle;
-        const genres = movie.genres.map(genre => genreMap.get(defaultClient[genre.id].shortName));
-    
-        const duration = movie.runtime;
-        return {
-            englishTitle: title,
-            originalTitle: ogtitle,
-            director: directors,
-            imdbID: imdbid,
-            releaseYear: movie.originalReleaseYear,
-            releaseDate: movie.originalReleaseDate || "",
-            runtime: duration,
-            ageCertification: movie.ageCertification,
-            genres: genres.sort()
-        };
-    }
-
     function getLinks() {
-        var output = [];
+        const output = [];
         document.querySelectorAll("h2.title-card-heading").forEach((e) => {
             if (e.parentNode.href) {
                 output.push(e.parentNode.href);
@@ -74,17 +263,17 @@ javascript: (async function () {
 
     function jsonToCsv(jsonData) {
         const csvRows = [];
-    
+
         /* SIMKL CSV format https:/*simkl.com/apps/import/csv/ */
         const headers = ["simkl_id", "TVDB_ID", "TMDB", "IMDB_ID", "MAL_ID", "Type", "Title", "Year", "LastEpWatched", "Watchlist", "WatchedDate", "Rating", "Memo"];
         csvRows.push(headers.join(','));
-    
+
         /* Mapping JSON keys to CSV headers */
         jsonData.forEach(row => {
             console.log(row);
             const csvRow = headers.map(header => {
                 let value = '';
-    
+
                 switch (header) {
                     case "IMDB_ID":
                         value = row.imdbID || '';
@@ -102,14 +291,14 @@ javascript: (async function () {
                     default:
                         value = row[header] || ''; /* For headers that directly match JSON keys */
                 }
-    
+
                 const escapedCell = ('' + value).replace(/"/g, '\\"'); /* Escape double quotes */
                 return `"${escapedCell}"`;
             });
-    
+
             csvRows.push(csvRow.join(','));
         });
-    
+
         return csvRows.join('\n');
     }
 
@@ -118,9 +307,9 @@ javascript: (async function () {
         console.log(csvData);
         const blob = new Blob([csvData], { type: 'text/csv' });
         const url = URL.createObjectURL(blob);
-    
+
         const fileName = 'watchlist.csv'; /* Change the file extension to .csv */
-    
+
         const link = document.createElement('a');
         link.setAttribute('href', url);
         link.setAttribute('download', fileName);
@@ -135,13 +324,12 @@ javascript: (async function () {
 
     async function main() {
         clearWatchlistData();
-        var links = getLinks();
-        console.log(links);
-        const movieDataPromises = links.map(fetchAndParseData);
-        const allMovieData = (await Promise.all(movieDataPromises)).filter(data => data !== null);
-    
-        localStorage.setItem("watchlistData", JSON.stringify(allMovieData));
-        console.log(allMovieData);
-        downloadCsvFile(allMovieData);
+        const movieData = await fetchAndParseData();
+
+        if (movieData !== null) {
+            localStorage.setItem("watchlistData", JSON.stringify(movieData));
+            console.log(movieData);
+            downloadCsvFile(movieData);
+        }
     }
 })()
