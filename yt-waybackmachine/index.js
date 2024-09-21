b03779bc46f967656b7e85389c230e4889f465ec
Diego Fleitas <DiegoFleitas@users.noreply.github.com>
2024-09-21 00:45:15 -0300

    Update index.js

--- yt-waybackmachine/index.js
+++ yt-waybackmachine/index.js
@@ -1,12 +1,12 @@
 (() => {
-let url = "https://web.archive.org/web/20130720113437oe_/http://wayback-fakeurl.archive.org/yt/";
-
-// Prompt for YouTube ID
-let youtubeId = prompt("Enter the YouTube ID:");
-
-// Append the new YouTube ID to the URL
-let newUrl = `${url}/${youtubeId}`;
-
-// Open the new URL in a new tab
-window.open(newUrl, "_blank");
+  let url = "https://web.archive.org/web/20130720113437oe_/http://wayback-fakeurl.archive.org/yt/";
+  
+  // Prompt for YouTube ID
+  let youtubeId = prompt("Enter the YouTube ID:");
+  
+  // Append the new YouTube ID to the URL
+  let newUrl = `${url}/${youtubeId}`;
+  
+  // Open the new URL in a new tab
+  window.open(newUrl, "_blank");
 })();
